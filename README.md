3039bbfa4d4ed95af0fa5f1bc4fdca5a9ec7382a
thomasmfx <thomasmoisesf@gmail.com>
2024-09-21 21:33:05 -0300

    🔧 Setup initial files and dependencies

---
+++ README.md
@@ -0,0 +1,3 @@
+# CV Application
+
+This is a simple application where users can input their information and generate a CV/resume.
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Icon from 'feather-icons-react'
+
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
