2208357c67b0443621fcaff00dc5eb829caa061e
thomasmfx <thomasmoisesf@gmail.com>
2024-09-24 18:15:54 -0300

    ✨ feat: Add data to use in CV

---
+++ src/models/resumeData.js
@@ -0,0 +1,110 @@
+let contactInformation = [
+  {
+    label: 'First Name',
+    inputType: 'text',
+    value: ''
+  },
+  {
+    label: 'Last Name',
+    inputType: 'text',
+    value: ''
+  },
+  {
+    label: 'Address',
+    inputType: 'text',
+    value: ''
+  },
+  {
+    label: 'Phone',
+    inputType: 'tel',
+    value: ''
+  },
+  {
+    label: 'Email',
+    inputType: 'email',
+    value: ''
+  },
+  {
+    label: 'LinkedIn',
+    inputType: 'url',
+    value: ''
+  },
+  {
+    label: 'GitHub',
+    inputType: 'url',
+    value: ''
+  }
+];
+
+let experience = [
+  {
+    label: 'Position',
+    inputType: 'text',
+    value: ''
+  },
+  {
+    label: 'Company',
+    inputType: 'text',
+    value: ''
+  },
+  {
+    label: 'Location',
+    inputType: 'text',
+    value: ''
+  },
+  {
+    label: 'Job Description',
+    inputType: 'textarea',
+    value: ''
+  },
+  {
+    label: 'Start Date',
+    inputType: 'date',
+    value: ''
+  },
+  {
+    label: 'End Date',
+    inputType: 'date',
+    value: ''
+  }
+];
+
+let education = [
+  {
+    label: 'Institution',
+    inputType: 'text',
+    value: ''
+  },
+  {
+    label: 'Course',
+    inputType: 'text',
+    value: ''
+  },
+  {
+    label: 'Location',
+    inputType: 'text',
+    value: ''
+  },
+  {
+    label: 'Start Date',
+    inputType: 'date',
+    value: ''
+  },
+  {
+    label: 'End Date',
+    inputType: 'date',
+    value: ''
+  }
+];
+
+let skills = ['HTML', 'CSS', 'Javascript', 'Jest', 'Webpack', 'React'];
+
+let languages = [];
+
+export  { 
+  contactInformation,
+  experience,
+  education,
+  skills,
+  languages
+}
