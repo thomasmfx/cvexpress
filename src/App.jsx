f590827362bcd3a56b2089a2df89e42a05b68a9b
thomasmfx <thomasmoisesf@gmail.com>
2024-09-26 13:12:55 -0300

    ✨ feat: Add base form and actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import { useState } from "react"
 import { Eye, Palette, Download, Pencil } from 'lucide-react';
+import { contactInformation, experience, education } from "./models/resumeData";
 import Header from "./components/Header"
 import TabButton from "./components/TabButton";
+import Toolbar from "./components/Toolbar";
+import Form from "./components/Form";
+import Input from "./components/Input";
+import SkillsForm from "./components/SkillsForm";
 
-const tabs = ['edit-resume', 'edit-styles', 'download-resume', 'view-resume']
+const tabs = [
+  {
+    name: 'edit-resume',
+    icon: <Pencil color="#212529" size='28px'/>,
+  },
+  {
+    name: 'edit-styles',
+    icon: <Palette color="#212529" size='28px'/>
+  },
+  {
+    name: 'download-resume',
+    icon: <Download color="#212529" size='28px'/>
+  },
+  {
+    name: 'view-resume',
+    icon: <Eye color="#212529" size='28px'/>
+  }
+];
 
 function App() {
   const [currentTab, setCurrentTab] = useState(tabs[0]);
@@ -14,36 +36,51 @@ function App() {
 
   return (
     <>
-      <Header>
-        <TabButton
-          tab={tabs[0]}
-          isCurrentTab={currentTab === tabs[0]} 
-          handleClick={() => handleChangeTab(tabs[0])}
-        >
-          <Pencil />
-        </TabButton>
-        <TabButton 
-          tab={tabs[1]}
-          isCurrentTab={currentTab === tabs[1]} 
-          handleClick={() => handleChangeTab(tabs[1])}
-        >
-          <Palette />
-        </TabButton>
-        <TabButton
-          tab={tabs[2]}
-          isCurrentTab={currentTab === tabs[2]} 
-          handleClick={() => handleChangeTab(tabs[2])}
-        >
-          <Download />
-        </TabButton>
-        <TabButton 
-          tab={tabs[3]}
-          isCurrentTab={currentTab === tabs[3]} 
-          handleClick={() => handleChangeTab(tabs[3])}
-        >
-          <Eye />
-        </TabButton>
-      </Header>
+      <Toolbar>
+        {tabs.map((tab) => 
+          <TabButton
+            key={tab.name}
+            tab={tabs.name}
+            isCurrentTab={currentTab === tab.name} 
+            handleClick={() => handleChangeTab(tab.name)}
+          >
+            {tab.icon}
+          </TabButton>
+        )}
+      </Toolbar>
+      <Header />
+      <main className="main">
+        <Form title={'Contact information'} >
+          {contactInformation.map((field) =>
+            <Input
+              key={field.label} 
+              label={field.label}
+              type={field.inputType}
+            />
+          )}
+        </Form>
+        <Form title={'Experience'} >
+          {experience.map((field) =>
+            <Input
+              key={field.label} 
+              label={field.label}
+              type={field.inputType}
+            />
+          )}
+        </Form>
+        <Form title={'Education'} >
+          {education.map((field) =>
+            <Input
+              key={field.label} 
+              label={field.label}
+              type={field.inputType}
+            />
+          )}
+        </Form>
+        <Form title={'Skills'} >
+          <SkillsForm />
+        </Form>
+      </main>
     </>
   )
 }
